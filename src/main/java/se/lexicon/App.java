73d3421e813760ea5f4c4d51d1c75c5dae708691
Mia1251 <svanteomax@gmail.com>
2020-12-15 11:29:37 +0100

    Update of previous file.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -6,12 +6,10 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        // get input from console
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your username: ");
-        // get string from console
+
         String username = scanner.nextLine();
-        // print message
         System.out.println("Hello " + username );
     }
 }
